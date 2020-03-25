4c6ac7027f3c821e00f8d9f962c5ea28c8bfd8cf
dev09 <devdevdevdevdev09@gmail.com>
2020-03-25 23:09:50 +0900

    자바스크립트 파일 읽기 샘플

--- slackChat/js/index.js
+++ slackChat/js/index.js
@@ -1,3 +1,34 @@
 $(function(){
-    alert("index.js, 테스트");
+    init();
 });
+
+const init = function(){
+    eventInit();
+}
+
+const eventInit = function(){
+    $("#fileReadBtn").on("click", openTextFile);
+}
+
+const openTextFile = function(){
+    const input = document.createElement("input");
+
+    input.type = "file";
+    input.accept = "text/plain";
+
+    input.onchange = function(e){
+        processFile(e.target.files[0]);
+    }
+
+    input.click();
+}
+
+const processFile = function(file){
+    const reader = new FileReader();
+
+    reader.onload = function(){
+        output.innerText = reader.result;
+    };
+
+    reader.readAsText(file, "utf-8");
+}
